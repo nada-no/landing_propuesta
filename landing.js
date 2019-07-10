08856229d20b7b2a9f759cfdaa490f26c1e2b5dd
stoyechounadan <stoyechounadan@hotmail.com>
2019-07-10 12:30:46 +0200

    slider terminado

---
+++ landing.js
@@ -0,0 +1,38 @@
+//JS PARA EL HEADER CAMBIANDO DE COLOR
+
+
+window.onscroll = function () {
+    colormenu()
+};
+
+function colormenu() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("menu_color").style.filter = "invert(100)";
+        document.getElementById("color").style.filter = "invert(100)";
+    } else {
+        document.getElementById("menu_color").style.filter = "none";
+        document.getElementById("color").style.filter = "none";
+
+    }
+}
+
+
+//JS PARA EL SLIDERRRR
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slider");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}

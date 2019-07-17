db2888e7eedbde58b69bde002dd0911d33d31290
stoyechounadan <stoyechounadan@hotmail.com>
2019-07-17 13:57:11 +0200

    finalizado con header mini

--- landing.js
+++ landing.js
@@ -20,34 +20,12 @@ function colormenu() {
 }
 
 
-//JS PARA EL SLIDERRRR
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-    // $(".slide").fadeIn();
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slider");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block"; 
-    
-}
-
 
 //FADE IN ESLOGAN PORTADA
 
 $(document).ready(function(){
  
-    $("h1").fadeIn("slow");
+    $("#claim h1").fadeIn("slow");
   
 });
 
